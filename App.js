3670b1a87f531507b0adcf475cbf11da85dff510
Kosarev Aleksey <KosarevA@rusfinance.ru>
2020-02-03 16:40:35 +0300

    InitialCommit

--- App.js
+++ App.js
@@ -1,45 +1,89 @@
 import React from 'react';
-import {render} from 'react-dom';
+import {Button, ScrollView, Text, View, StyleSheet, Switch} from 'react-native';
 
-const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center',
-};
+let id = 0;
 
-const Todo = props => {
-  <li>
-    <input type="checkbox" />
-    <button>delete</button>
-    <span>{props.text}</span>
-  </li>
-}
+const Todo = props => (
+  <View style={styles.todoContainer}>
+    <Switch onValueChange={props.onToggle} value={props.todo.checked} />
+    <Button onPress={props.onDelete} title="delete" />
+    <Text>{props.todo.text}</Text>
+  </View>
+);
 
-class App extends React.Component {
+const styles = StyleSheet.create({
+  todoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 3,
+  },
+  appContainer: {
+    flex: 1,
+    paddingTop: 30,
+  },
+  fill: {
+    flex: 1
+  }
+});
+
+export default class App extends React.Component {
+
   constructor() {
     super();
     this.state = {
       todos: [],
     };
   }
-  addTodo(){
-    const text = prompt('TODO text please!');
+
+  addTodo() {
+    id++;
+    const text = `TODO number ${id}`;
     this.setState({
-      todos: [...this.state.todos, {text: text}],
+      todos: [
+        ...this.state.todos,
+        {id: id, text: text, checked: false}
+      ]
     });
+  }
+
+  removeTodo(id){
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked,
+        }
+      })
+    })
   }
 
   render() {
     return (
-      <div>
-        <button>Add todo</button>
-        <ul>
+      <View style={[styles.appContainer, styles.fill]}>
+        <Text>Todo count: {this.state.todos.length}</Text>
+        <Text>
+          Unchecked todo count{' '}
+          {this.state.todos.filter(todo => !todo.checked).length}
+        </Text>
+        <Button onPress={() => this.addTodo()} title="Add todo" />
+        <ScrollView style={styles.fill}>
           {this.state.todos.map(todo => (
-            <Todo todo="todo" />
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
           ))}
-        </ul>
-      </div>
+        </ScrollView>
+      </View>
     );
   }
 }
-
-export default App;
