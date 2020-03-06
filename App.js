bb5c3b4ad342ef26108aba9a92b557819c8cb5e6
Kosarev Aleksey <KosarevA@rusfinance.ru>
2020-03-06 18:32:44 +0300

    ListView

--- App.js
+++ App.js
@@ -1,81 +1,37 @@
 import React from 'react';
-import {Button, ScrollView, Text, View, StyleSheet, Switch} from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  appContainer:{
-    
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  count: {
-    fontSize: 48,
-  }
-})
+import contacts from './contacts' // just an array of contacts
+import Row from './Row'
 
-
-class Counter extends React.Component {
-
-  constructor(){
-    super()
-    this.state = {
-      count:0,
-    }
+export default class App extends React.Component {
+  state = {
+    showContacts: false,
   }
 
-  inc(){
-    console.log("increment!")
-    this.setState(prevState=>({
-      count: prevState.count + 1,
-    }))
-  }
-
-  componentWillUnmount(){
-    clearInterval(this.interval)
-  }
-
-  componentDidMount(){
-    this.interval = setInterval(() => {
-      this.inc();
-    }, 1000);
+  toggleContacts = () => {
+    this.setState(prevState => ({ showContacts: !prevState.showContacts }))
   }
 
   render() {
     return (
-      <View style={styles.appContainer}>
-        <Text style={styles.count}> {this.state.count} </Text>
+      <View style={styles.container}>
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
+        {this.state.showContacts && (
+          <ScrollView>
+            {contacts.map(contact => <Row key={contact.key} {...contact} />)}
+          </ScrollView>
+        )}
       </View>
-    );
-  }
 
-}
-
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      showCounter: true
-    }
-  }
-  toggleCounter = () => this.setState(prevState => ({
-    showCounter: !prevState.showCounter,
-  }))
-
-  render(){
-    if (this.state.showCounter) {
-      return (
-        <View style={styles.appContainer}>
-          <Button title="toggle" onPress={this.toggleCounter} />
-          <Counter />
-        </View>
-      );
-    } else {
-      return(
-        <View style={styles.appContainer}>
-          <Button title="toggle" onPress={this.toggleCounter} />
-        </View>
-      );
-    }
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop:2,
+  },
+});
