ee470556bd1fe5036a4b4ebe17852ab9dc545636
murmilad <murmilad@gmail.com>
2022-01-07 03:47:59 +0300

    use csv-database for resume

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { exec } = require('child_process')
 const sizeOf = require('image-size')
+const csvdb = require('csv-database')
 
 const PORT = process.env.PORT || 8000
 
 const GALLERY_PATH = './gallery'
 const RESUME_FOLDER = 'resume'
-const RESUME_FILE = 'resume.txt'
+const RESUME_FILE = 'resume_test.txt'
 
 function getResume() {
   let resume = {};
@@ -21,187 +22,194 @@ function getResume() {
 
   return resume;
 }
-const app = express()
-app.use(bodyParser.json())
 
-var fs = require('fs');
+csvdb(GALLERY_PATH + '/' + RESUME_FOLDER + '/' + RESUME_FILE, ["name","resume"], "|").then(async db => {
 
-// Login
+  const app = express()
+  app.use(bodyParser.json())
 
-// usernames are keys and passwords are values
-const users = {
-  username: 'password',
-}
+  var fs = require('fs');
 
-app.post('/login', (req, res) => {
-  const {username, password} = req.body
+  // Login
 
-  if (!username || !password) return res.status(400).send('Missing username or password')
-  // in practice, this is potentially revealing too much information.
-  // an attacker can probe the server to find all of the usernames.
-  if (!users[username]) return res.status(403).send('User does not exist')
-  if (users[username] !== password) return res.status(403).send('Incorrect password')
-  return res.json({token:'thisIsARealToken'})
-})
+  // usernames are keys and passwords are values
+  const users = {
+    username: 'password',
+  }
+
+  app.post('/login', (req, res) => {
+    const {username, password} = req.body
+
+    if (!username || !password) return res.status(400).send('Missing username or password')
+    // in practice, this is potentially revealing too much information.
+    // an attacker can probe the server to find all of the usernames.
+    if (!users[username]) return res.status(403).send('User does not exist')
+    if (users[username] !== password) return res.status(403).send('Incorrect password')
+    return res.json({token:'thisIsARealToken'})
+  })
 
 
-// Gallery
+  // Gallery
 
-app.get('/gallery', (req, res) => {
+  app.get('/gallery',async (req, res) =>  {
 
-  let resume = getResume()
+    let resume = await db.get();
 
-  let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER;
-  let gallery = [];
-  fs.readdirSync(albumPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
-    .sort().reverse()
-    .forEach(main_folder => {
+    let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER
+    let gallery = []
+    fs.readdirSync(albumPath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+      .sort().reverse()
+      .forEach(main_folder => {
 
-      let imageCount = 0;
-      let signedImageCount = 0;
-      let thumbnails = [];
-      fs.readdirSync(albumPath + '/' + main_folder, { withFileTypes: true })
-        .filter(dirent => dirent.isDirectory())
-        .map(dirent => dirent.name)
-        .forEach(folder => {
+        let imageCount = 0;
+        let signedImageCount = 0;
+        let thumbnails = [];
+        fs.readdirSync(albumPath + '/' + main_folder, { withFileTypes: true })
+          .filter(dirent => dirent.isDirectory())
+          .map(dirent => dirent.name)
+          .forEach(folder => {
 
-          fs.readdirSync(albumPath + '/' + main_folder + '/' + folder)
-            .forEach( file => {
-              thumbnails.push(file)
+            fs.readdirSync(albumPath + '/' + main_folder + '/' + folder)
+              .forEach( async file => {
+                thumbnails.push(file)
 
-              if (resume[file]) signedImageCount++
-              imageCount++
-            })
+                let foundResume  = resume.find((imageName) => imageName.name === file)
+                if (foundResume && foundResume.resume) signedImageCount++
+                imageCount++
+              })
+          })
+
+        let albumData = {}
+
+        albumData.thumbnail_name = thumbnails
+        albumData.name = main_folder
+        albumData.header = main_folder.replace(/^\w+_/,'').replaceAll(/-/g,' ')
+        albumData.imageCount = imageCount
+        albumData.unsignedImageCount = imageCount - signedImageCount
+        gallery.push(albumData)
+
+
+      })
+
+    return res.json(gallery)
+  })
+
+
+  // Album
+
+  app.get('/album/:album', async (req, res) => {
+    
+
+    if (!req.params.album) return res.status(400).send('Missing album')
+
+    
+    let resume = await db.get();
+
+    let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER + '/' + req.params.album;
+    let albumList = [];
+    fs.readdirSync(albumPath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+      .forEach(folder => {
+
+        fs.readdirSync(albumPath + '/' + folder).forEach(file => {
+
+          let imageData = {}
+          let foundResume  = resume.find((imageName) => imageName.name === file)
+
+          imageData.thumbnail_name = file
+          imageData.name = file
+          imageData.albumName = req.params.album
+          imageData.imageName = file
+          imageData.resume = foundResume ? foundResume.resume : undefined
+          albumList.push(imageData)
         })
-
-      let albumData = {}
-
-      albumData.thumbnail_name = thumbnails
-      albumData.name = main_folder
-      albumData.header = main_folder.replace(/^\w+_/,'').replaceAll(/-/g,' ')
-      albumData.imageCount = imageCount
-      albumData.unsignedImageCount = imageCount - signedImageCount
-      gallery.push(albumData)
-
-
     })
 
-  return res.json(gallery)
-})
-
-
-// Album
-
-app.get('/album/:album', (req, res) => {
-  
-
-  if (!req.params.album) return res.status(400).send('Missing album')
-
-  let resume = getResume()
-
-
-  let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER + '/' + req.params.album;
-  let albumList = [];
-  fs.readdirSync(albumPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
-    .forEach(folder => {
-
-      fs.readdirSync(albumPath + '/' + folder).forEach( file => {
-
-        let imageData = {}
-
-        imageData.thumbnail_name = file
-        imageData.name = file
-        imageData.albumName = req.params.album
-        imageData.imageName = file
-        imageData.resume = resume[file]
-        albumList.push(imageData)
-      })
+    return res.json(albumList)
   })
 
-  return res.json(albumList)
-})
+  // Image
 
-// Image
+  app.get('/album/:album/image/:image',  (req, res) => {
+    
 
-app.get('/album/:album/image/:image', (req, res) => {
-  
-
-  if (!req.params.album) return res.status(400).send('Missing album')
-  if (!req.params.image) return res.status(400).send('Missing image')
+    if (!req.params.album) return res.status(400).send('Missing album')
+    if (!req.params.image) return res.status(400).send('Missing image')
 
 
-  let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER + '/' + req.params.album;
+    let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER + '/' + req.params.album;
 
-  let file = req.params.image
-  let imageData = {}
-  fs.readdirSync(albumPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
-    .forEach(folder => {
+    let file = req.params.image
+    let imageData = {}
+    fs.readdirSync(albumPath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+      .forEach(folder => {
 
-      if (fs.existsSync(albumPath + '/' + folder + '/' + file)) {
-        let bitmap = fs.readFileSync(albumPath + '/' + folder + '/' + file)
-        let dimensions = sizeOf(albumPath + '/' + folder + '/' + file)
+        if (fs.existsSync(albumPath + '/' + folder + '/' + file)) {
+          let bitmap = fs.readFileSync(albumPath + '/' + folder + '/' + file)
+          let dimensions = sizeOf(albumPath + '/' + folder + '/' + file)
 
-        imageData.thumbnail = Buffer.from(bitmap).toString('base64')
-        imageData.width = dimensions.width
-        imageData.height = dimensions.height
-        imageData.name = file
-      }
+          imageData.thumbnail = Buffer.from(bitmap).toString('base64')
+          imageData.width = dimensions.width
+          imageData.height = dimensions.height
+          imageData.name = file
+        }
+    })
+
+    return res.json(imageData)
   })
 
-  return res.json(imageData)
-})
+  // Art
 
-// Art
+  app.get('/album/:album/art/:art', async (req, res) => {
+    
 
-app.get('/album/:album/art/:art', (req, res) => {
-  
+    if (!req.params.album) return res.status(400).send('Missing album')
+    if (!req.params.art) return res.status(400).send('Missing art')
 
-  if (!req.params.album) return res.status(400).send('Missing album')
-  if (!req.params.art) return res.status(400).send('Missing art')
 
-  let resume = getResume()
+    let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER + '/' + req.params.album;
 
-  let albumPath = GALLERY_PATH + '/' + RESUME_FOLDER + '/' + req.params.album;
+    let file = req.params.art
+    let foundResume = await db.get({name: file});
+    let imageData = {}
+    fs.readdirSync(albumPath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+      .forEach(folder => {
 
-  let file = req.params.art
-  let imageData = {}
-  fs.readdirSync(albumPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
-    .forEach(folder => {
+        if (fs.existsSync(albumPath + '/' + folder + '/' + file)) {
+          let bitmap = fs.readFileSync(albumPath + '/' + folder + '/' + file)
+          let dimensions = sizeOf(albumPath + '/' + folder + '/' + file)
 
-      if (fs.existsSync(albumPath + '/' + folder + '/' + file)) {
-        let bitmap = fs.readFileSync(albumPath + '/' + folder + '/' + file)
-        let dimensions = sizeOf(albumPath + '/' + folder + '/' + file)
+          imageData.thumbnail = Buffer.from(bitmap).toString('base64')
+          imageData.width = dimensions.width
+          imageData.height = dimensions.height
+          imageData.name = file
+          imageData.resume = foundResume.length > 0 ? foundResume[0].resume : undefined
+        }
+    })
 
-        imageData.thumbnail = Buffer.from(bitmap).toString('base64')
-        imageData.width = dimensions.width
-        imageData.height = dimensions.height
-        imageData.name = file
-        imageData.resume = resume[file]
-      }
+    return res.json(imageData)
   })
 
-  return res.json(imageData)
+  // catch 404
+  app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  })
+
+  app.use((err, req, res, next) => res.status(err.status || 500).send(err.message || 'There was a problem'))
+
+  const server = app.listen(PORT)
+  console.log(`Listening at http://localhost:${PORT}`)  
 })
 
-// catch 404
-app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-app.use((err, req, res, next) => res.status(err.status || 500).send(err.message || 'There was a problem'))
-
-const server = app.listen(PORT)
-console.log(`Listening at http://localhost:${PORT}`)
 
 process.on('SIGINT', function() {
   console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
